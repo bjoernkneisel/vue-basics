ffb0192432f7e47c3cd7657e0b10c9c6636c7000
bjoernkneisel <info@bjoern-kneisel.de>
2020-10-05 11:57:40 +0200

    More basics

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="shoppingList">
     <h2 class="title is-1"> {{ header.toLocaleUpperCase() }} </h2>
+    <p class="title is-4" v-if="shoppingItems.length === 0">Glückwunsch, du hast keine Sachen auf dem Einkaufszettel!</p>
     <div class="columns">
       <div class="column"></div>
       <div class="column">
@@ -10,21 +11,22 @@
         <input class="subtitle" v-model="newItem.name" type="text" placeholder="Füge eine Sache hinzu">
       </div>
       <div class="column">
-        <button class="button is-primary" @click="saveItem()">Sache speichern</button>
+        <button class="button is-primary" :disabled="newItem.length === 0" @click="saveItem()">Sache speichern</button>
       </div>
       <div class="column"></div>
     </div>
-    <div class="columns" v-for="item in shoppingItems" :key="item.name">
+    <div class="columns" id="nichtMarkierbar" v-for="item in shoppingItems" :key="item.name">
       <div class="column is-one-third"></div>
       <div class="column">
-        <p class="subtitle is-6 has-text-right"> {{ item.count }} x</p>
+        <p class="subtitle is-6 has-text-right" :class="{strikeout: item.purchased}"> {{ item.count }} x</p>
       </div>
-      <div class="column">
-        <p class="subtitle is-6 has-text-left"> {{ item.name}}</p>
+      <div class="column" @click="item.purchased = !item.purchased">
+        <p class="subtitle is-6 has-text-left" :class="{strikeout: item.purchased}"> {{ item.name}}</p>
+        
       </div>
       <div class="column is-one-third"></div>
     </div>
-  </div>
+  </div> 
 </template>
 
 <script>
@@ -32,21 +34,35 @@ export default {
   data() {
     return {
       shoppingItems: [
-        { count: '20', name: 'Partyhüte'},
-        { count: '20', name: 'Plastikbecher'},
-        { count: '2', name: 'Brettspiele'},
+        // { count: '20', name: 'Partyhüte'},
+        // { count: '20', name: 'Plastikbecher'},
+        // { count: '2', name: 'Brettspiele'},
     ],
     newItem: [
-      { count: '', name: ''}
+      { id: '', count: '', name: ''}
     ],
     header: 'Einkaufsliste'
     }
   },
   methods: {
     saveItem: function() {
-      this.shoppingItems.push({count: this.newItem.count, name: this.newItem.name});
+      this.newItem.id = this.shoppingItems.length;
+      this.shoppingItems.push({id: this.newItem.id, count: this.newItem.count, name: this.newItem.name, purchased: false});
       this.newItem = [ { count: '', name: ''} ];
-    }
+    },
   }
 }
 </script>
+
+<style>
+.strikeout {
+  text-decoration: line-through;
+}
+
+#nichtMarkierbar {
+  -ms-user-select: None;
+  -moz-user-select: None;
+  -webkit-user-select: None;
+  user-select: None;
+}
+</style>
